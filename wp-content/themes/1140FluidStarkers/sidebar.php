44a91f1dda2b8b487b43ff12b7ee82205f867f61
thelando618 <landonzirkelbach@gmail.com>
2012-08-25 20:30:27 -0500

    Fixing page templates. taking out extra crap

--- wp-content/themes/1140FluidStarkers/sidebar.php
+++ wp-content/themes/1140FluidStarkers/sidebar.php
@@ -18,25 +18,6 @@
 	 */
 	if ( ! dynamic_sidebar( 'primary-widget-area' ) ) : ?>
 	
-			<li>
-				<?php get_search_form(); ?>
-			</li>
-
-			<li>
-				<h3><?php _e( 'Archives', 'twentyten' ); ?></h3>
-				<ul>
-					<?php wp_get_archives( 'type=monthly' ); ?>
-				</ul>
-			</li>
-
-			<li>
-				<h3><?php _e( 'Meta', 'twentyten' ); ?></h3>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<?php wp_meta(); ?>
-				</ul>
-			</li>
 
 		<?php endif; // end primary widget area ?>
 			</ul>
